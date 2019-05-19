7c929ddfd88bab96323cc951cc09c9a334fd2479
Oleksandr Mertsalov <sashamercalovv@gmail.com>
2019-05-19 18:46:19 +0200

    Updating MonthStatisticController

--- acount-service2/src/main/java/com/acountservice2/controllers/MonthStatisticsController.java
+++ acount-service2/src/main/java/com/acountservice2/controllers/MonthStatisticsController.java
@@ -2,6 +2,7 @@ package com.acountservice2.controllers;
 
 import com.acountservice2.entities.Spending;
 import com.acountservice2.entities.User;
+import com.acountservice2.formobjects.StatisticForm;
 import com.acountservice2.services.SpendingService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,14 +25,42 @@ public class MonthStatisticsController {
 
     @Autowired
     SpendingService spendingService;
-    @GetMapping("/spendingByCategory")
-    public String getMonthlySpendingsByCategoryStats(ModelMap modelMap, @AuthenticationPrincipal User user){
+    @GetMapping("/spendingByCategoryCurrMon")
+    public String getCurrentMonthlySpendingsByCategoryStats(ModelMap modelMap, @AuthenticationPrincipal User user){
         List<Spending> spendingList =spendingService.getSpendingForMonthOfYear(4, 2019, user);
-        Map<String, Double> spendMap;
-        spendMap = spendingList.stream().
+        Map<String, Double> spendMap = getCategorySumPair(spendingList);
+        modelMap.addAttribute("spendMap", spendMap);
+
+        StatisticForm statForm = new StatisticForm();
+        modelMap.addAttribute("statForm", statForm);
+
+        return "spendCatStats";
+    }
+
+    @GetMapping("/spendingByCategory")
+    public String getSpendingsByCategoryStats(ModelMap modelMap,
+                                              StatisticForm statForm, @AuthenticationPrincipal User user){
+
+        List<Spending> spendingList =null;
+        if(statForm.getIsYear()){  //get spending for whole year
+            spendingList=spendingService.getListSpendingForYear(user, statForm.getYear());
+        }
+        else {
+            spendingList=spendingService.getSpendingForMonthOfYear(statForm.getMonth(), statForm.getYear(), user);
+        }
+
+        Map<String, Double> spendMap = getCategorySumPair(spendingList);
+        modelMap.addAttribute("spendMap", spendMap);
+
+        modelMap.addAttribute("statForm", statForm);
+
+        return "spendCatStats";
+    }
+    private Map<String, Double> getCategorySumPair(List<Spending> spendingList){
+        return spendingList.stream().
                 collect(groupingBy(spending -> spending.getCategory().getName(),
                         summingDouble(Spending::getAmount)));
-        modelMap.addAttribute("spendMap", spendMap);
-        return "spendCatStats";
+
+
     }
 }
